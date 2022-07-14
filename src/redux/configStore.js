a9064f86c3cb4d60953e1fb0cf8c33163afef1bb
jm03100 <107831631+jm03100@users.noreply.github.com>
2022-07-14 15:43:16 +0900

    Update configStore.js

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -3,8 +3,6 @@ import thunk from "redux-thunk";
 import card from './modules/card';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-// export const history = createBrowserHistory();
-
 const middlewares = [thunk];
 
 const enhancer = applyMiddleware(...middlewares);
